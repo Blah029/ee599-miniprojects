5f4123622ad580281f840965eb984946e5b208d9
Blah029 <81973454+Blah029@users.noreply.github.com>
2023-08-23 16:03:50 +0530

    Plot full pitch variation

--- ee599_a1_main.py
+++ ee599_a1_main.py
@@ -6,6 +6,10 @@ Group:
     -
     -
     -
+
+References:
+    - https://librosa.org/doc/main/generated/librosa.lpc.html
+    - https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.lfilter.html
 """
 import librosa
 import logging
@@ -26,22 +30,47 @@ in_path = "data"
 in_file = "sarigamapa.wav"
 yy, f_s = librosa.load(f"{in_path}\\{in_file}")
 ## Properties
-window_size = f_s*0.25
-window_gap = f_s*0.05
-window_count = math.ceil(len(yy)/window_size)
+window_size = int(f_s*0.1)
+window_gap = int(window_size*0.25)
+window_count = math.floor((len(yy)-window_size)/window_gap) + 1
+pole_count = 16
 logger.debug(f"sample rate:     {f_s} Hz")
 logger.debug(f"samples:         {len(yy)}")
 logger.debug(f"duration:        {len(yy)/f_s} s")
-logger.debug(f"window size:     {window_size/f_s*1000:.0f} ms")
-logger.debug(f"window overlap:  {window_gap/window_size*100:.0f} %")
-## Analysis
-y_trimmed = yy[:f_s]
-aa = librosa.lpc(y_trimmed, order=128) # coefficients of lpc function
-xx = np.zeros_like(y_trimmed)
-xx[0] = 1
-y_hat = signal.lfilter(np.array([1]),aa,xx)
-f_poles = f_s*np.sort(np.angle(np.roots(aa)))/(2*np.pi)
-f_poles_abs = np.sort(np.abs(f_poles))
-f_poles_abs = f_poles_abs[f_poles_abs != 0]
-logger.info(f"fundamental: {f_poles_abs[1]} Hz")
+logger.debug(f"window size:     {window_size} ({window_size/f_s*1000:.0f} ms)")
+logger.debug(f"window gap:      {window_gap}")
+logger.debug(f"window overlap:  {window_size - window_gap} ({window_gap/window_size*100:.0f} %)")
+logger.debug(f"winodw count:    {window_count}")
+## Analyse
+pitch = []
+y_hat = np.zeros_like(yy) 
+for i in range(window_count):
+    y_trimmed = yy[i*window_gap:i*window_gap + window_size]
+    aa = librosa.lpc(y_trimmed, order=pole_count) # coefficients of lpc function
+    xx = np.zeros_like(y_trimmed)
+    xx[0] = 1
+    y_hat_trimmed = signal.lfilter(np.array([1]),aa,xx)
+    y_hat[i*window_gap:i*window_gap + window_size] = y_hat_trimmed
+    f_poles = f_s*np.sort(np.abs(np.angle(np.roots(aa))))/(2*np.pi)
+    for f_p in f_poles:
+        if f_p != 0:
+            pitch.append(f_p)
+            break
+    ## Plot
+    if i == 0:
+        logger.info(f"starting fundamental: {f_poles[1]:.2f} Hz")
+        fig,ax = plt.subplots(2,2, num="Characteristics of 1st Window")
+        fig.tight_layout()
+        ax[0,0].title.set_text("LPC Coefficients")
+        ax[0,0].bar(np.linspace(1,len(aa),len(aa)),aa,1)
+        ax[0,1].title.set_text("Input Impulse")
+        ax[0,1].stem(xx)
+        ax[1,0].title.set_text("Synthesised Signal")
+        ax[1,0].plot(y_hat_trimmed)
+        ax[1,1].title.set_text("Frequencies of Poles (Magnitude)")
+        ax[1,1].stem(f_poles)
+plt.figure("Variation of Pitch")
+plt.plot(np.array(pitch))
+plt.show()
+
 
